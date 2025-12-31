dbddd9ceca2ca9a365cf20fe4151de9ecb62d0d0
JY尘 <JY4k.api@gmail.com>
2025-12-31 17:30:28 +0800

    优化管理员功能

--- api/manage.js
+++ api/manage.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const isAuth = token === config.adminToken;
   const currentVersion = config.currentVersion;
 
-  // --- API: 保存数据并触发部署 ---
+  // --- API:  保存数据并触发部署 ---
   if (req.method === 'POST') {
     if (!isAuth) return res.status(401).json({ error: '无权操作' });
 
